e1b6864f2011b53e68db227407cbf1c57614bb97
Greg P <gperlman27@gmail.com>
2017-04-09 21:49:05 -0400

    chords, left, right basic implementation. whitespace separator fix

--- src/components/app.js
+++ src/components/app.js
@@ -9,27 +9,61 @@ export default class App extends Component {
     conductor.setTimeSignature(4,4);
     conductor.setTempo(120);
 
-    const measurement = 'sixteenth';
+    const measurement = 'eighth';
 
-    const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    const text = `Shall I compare thee to a summer's day?
+                  Thou art more lovely and more temperate:
+                  Rough winds do shake the darling buds of May,
+                  And summer's lease hath all too short a date:
+                  Sometimes too hot the eye of heaven shines,
+                  And too often is his gold complexion dimm'd:
+                  And every fair from fair sometimes declines,
+                  By chance or natures changing course untrimm'd;
+                  By thy eternal summer shall not fade,
+                  Nor lose possession of that fair thou owest;
+                  Nor shall Death brag thou wander'st in his shade,
+                  When in eternal lines to time thou growest:
+                  So long as men can breathe or eyes can see,
+                  So long lives this and this gives life to thee.`;
 
     // let words = text.split(" ");
 
-    let musicFromText = text.split(" ").map(function(word){
+    let musicFromText = text.split(/\s+/).map(function(word){
       return wordsToMusic(measurement, word);
     });
 
-    // flatten array to loop through chords/notes
-
-    musicFromText = [].concat.apply([], musicFromText);
-
     console.log(musicFromText);
 
-    var piano = conductor.createInstrument();
+    // flatten array to loop through chords/notes
+
+    // musicFromText = [].concat.apply([], musicFromText);
+
+    // console.log(musicFromText);
+
+    var rightHand = conductor.createInstrument();
+    var leftHand  = conductor.createInstrument();
+    var chord = conductor.createInstrument();
 
     musicFromText.forEach(function(music){
-      piano.note(music.measurement, music.note);
+      if(music.notes){
+        music.notes.forEach(function(note){
+          if(note.hand === 'right'){
+            console.log('right');
+            rightHand.note(note.measurement, note.note);
+          } else {
+            console.log('left');
+            leftHand.note(note.measurement, note.note);
+          }
+        })
+      } else {
+        // default chords to righthand
+        // chord.note('quarter', music.note);
+        rightHand.note('half', music.note);
+        leftHand.rest('half');
+      }
     })
+
+    console.log(rightHand, leftHand, chord);
 
     // piano.note('sixteenth', 'Bb4');
     // piano.note('sixteenth', 'D4');
--- src/transpose.js
+++ src/transpose.js
@@ -4,10 +4,73 @@ const wordsToMusic = (measurement, word) => {
   // console.log(word);
   // console.log(CODE);
   const letters = word.split("");
+  const regex = /([A-Z])|([.!?])/;
 
-  return letters.map((letter) => {
-    return { measurement: measurement, note: CODE[letter] };
+  if(regex.test(word)){
+    return { measurement: 'quarter', note: chord(letters) };
+  } else {
+    let left = 0, right = 0;
+    let notes = singleNote(measurement, letters);
+
+    // get sums of notes so we know how to measure them
+    notes.forEach(function(note){
+      if(note.hand === 'right'){
+        right++;
+      } else {
+        left++;
+      }
+    })
+
+    // add measurements to notes
+    notes.forEach(function(note){
+      if(note.hand === 'right'){
+        note.measurement = getMeasurements(right);
+      } else {
+        note.measurement = getMeasurements(left);
+      }
+    })
+
+    return { notes, left, right };
+  }
+}
+
+// return a chord from letters coming in
+const chord = (letters) => {
+  let chord = '';
+
+  letters.forEach(function(letter){
+    console.log(letter);
+    console.log(CODE[letter]);
+    chord += `${CODE[letter].note},`;
   });
+
+  // console.log(chord.replace(/,\s*$/, ""));
+
+
+  return chord.replace(/,\s*$/, "");
+}
+
+// return one note per letter coming in
+const singleNote = (measurement, letters) => {
+  return letters.map((letter) => {
+    return { note: CODE[letter].note, hand: CODE[letter].hand };
+  });
+}
+
+const getMeasurements = (n) => {
+  if(n <= 1){
+    return 'whole';
+  } else if(n <= 2){
+    return 'half';
+  } else if(n <= 4){
+    return 'quarter';
+  } else if(n <= 8){
+    return 'eighth';
+  } else if(n <= 16){
+    return 'sixteenth';
+  } else if(n <= 32){
+    return 'thirtySecond';
+  }
 }
 
 export {
